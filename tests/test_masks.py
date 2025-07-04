2edf6921844ef1d99c1b2d7fe348330fa29544d4
Sokolowsky_Konstantin <sokolowsky.konstantin@yandex.ru>
2025-07-04 17:06:10 +0500

    fixture for masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,8 +2,8 @@ import pytest
 
 from src.masks import get_mask_card_number, get_mask_account
 
-def test_get_mask_card_number():
-    assert get_mask_card_number('7000792289606361') == '7000 79** **** 6361'
+def test_get_mask_card_number(card):
+    assert get_mask_card_number('7000792289606361') == card
 
-def test_get_mask_account():
-    assert get_mask_account('Счет 64686473678894779589') == 'Счет **9589'
+def test_get_mask_account(score):
+    assert get_mask_account('Счет 64686473678894779589') == score
