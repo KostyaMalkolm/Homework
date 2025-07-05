faa8ebc60d22ae2981763a924ca37a9a02753369
Sokolowsky_Konstantin <sokolowsky.konstantin@yandex.ru>
2025-07-05 20:18:32 +0500

    create __int__.py, add tests for masks

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,16 @@
 import pytest
 
+import sys
+
+import os
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
 @pytest.fixture
 def card():
     return '7000 79** **** 6361'
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,9 +1,9 @@
-import pytest
-
 from src.masks import get_mask_card_number, get_mask_account
+
 
 def test_get_mask_card_number(card):
     assert get_mask_card_number('7000792289606361') == card
+
 
 def test_get_mask_account(score):
     assert get_mask_account('Счет 64686473678894779589') == score
