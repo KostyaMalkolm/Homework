7aa098208d3695b94d5187490be8860852b57803
Sokolowsky_Konstantin <sokolowsky.konstantin@yandex.ru>
2025-07-22 20:21:23 +0500

    create test_by_date, test_processing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,23 @@
 import pytest
 
-import sys
-
-import os
-
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-
-
-from src.masks import get_mask_card_number, get_mask_account
-
 
 @pytest.fixture
-def card():
+def card() -> str:
     return '7000 79** **** 6361'
 
 
 @pytest.fixture
-def score():
+def score() -> str:
     return 'Счет **9589'
+
+@pytest.fixture
+def date() -> str:
+    return '11.03.2024'
+
+@pytest.fixture
+def operations() -> str:
+    return '[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]'
+
+@pytest.fixture
+def sort_date() -> str:
+    return ''
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,9 +1,9 @@
 from src.masks import get_mask_card_number, get_mask_account
 
 
-def test_get_mask_card_number(card):
-    assert get_mask_card_number('7000792289606361') == card
+def test_get_mask_card_number(card: str) -> None:
+    assert get_mask_card_number("7000792289606361") == card
 
 
-def test_get_mask_account(score):
+def test_get_mask_account(score: str) -> None:
     assert get_mask_account('Счет 64686473678894779589') == score
