29908cbfe649d36509202061225009e214da2120
Sokolowsky_Konstantin <sokolowsky.konstantin@yandex.ru>
2025-08-09 21:04:09 +0500

    add test for sort_by_date

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,6 +23,7 @@ def filter_result_operations() -> List[Dict]:
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
+
 @pytest.fixture
 def filter_operations() -> List[Dict]:
     return [
@@ -34,5 +35,10 @@ def filter_operations() -> List[Dict]:
 
 
 @pytest.fixture
-def sort_date() -> str:
-    return ''
+def sort_result_date() -> list[dict[str, int | str]]:
+    return [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+]
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -4,5 +4,6 @@ from src.processing import filter_by_state, sort_by_date
 def test_filter_by_state(filter_operations, filter_result_operations):
     assert filter_by_state(filter_operations) == filter_result_operations
 
-# def test_sort_by_date()
-#
+
+def test_sort_by_date(filter_operations, sort_result_date):
+    assert sort_by_date(filter_operations) == sort_result_date
