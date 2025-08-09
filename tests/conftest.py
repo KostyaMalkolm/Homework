0436f4c7a25bb4b3330cb4ea84967f2661694d40
Sokolowsky_Konstantin <sokolowsky.konstantin@yandex.ru>
2025-08-09 21:37:02 +0500

    fix errors of annotation

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,20 +25,20 @@ def filter_result_operations() -> List[Dict]:
 
 
 @pytest.fixture
-def filter_operations() -> List[Dict]:
+def filter_operations() -> list[Dict]:
     return [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
 @pytest.fixture
-def sort_result_date() -> list[dict[str, int | str]]:
+def sort_result_date() -> list[Dict]:
     return [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-]
+    ]
